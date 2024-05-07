9ebdb65a3dc842917248ad12250df122777659c9
Jarek Syrylak <hedges@resync.pl>
2024-05-07 12:56:51 +0100

    Changes for GL build.

--- build.cmd
+++ build.cmd
@@ -69,47 +69,48 @@ pushd angle.src
 
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 call gclient sync || exit /b 1
-call gn gen out/ReleaseDX --args="angle_build_all=false is_debug=false angle_has_frame_capture=false angle_enable_gl=false angle_enable_vulkan=false angle_enable_d3d9=false angle_enable_null=false" || exit /b 1
-rem call gn gen out/ReleaseGL --args="angle_build_all=false is_debug=false angle_has_frame_capture=false angle_enable_gl=true angle_enable_vulkan=false angle_enable_d3d9=false angle_enable_d3d11=false angle_enable_null=false" || exit /b 1
+rem call gn gen out/ReleaseDX --args="angle_build_all=false is_debug=false angle_has_frame_capture=false angle_enable_gl=false angle_enable_vulkan=false angle_enable_d3d9=false angle_enable_null=false" || exit /b 1
+call gn gen out/ReleaseGL --args="angle_build_all=false is_debug=false angle_has_frame_capture=false angle_enable_gl=true angle_enable_vulkan=false angle_enable_d3d9=false angle_enable_d3d11=false angle_enable_null=false" || exit /b 1
 rem call gn gen out/Debug --args="angle_build_all=false is_debug=true angle_has_frame_capture=false angle_enable_gl=true angle_enable_vulkan=false angle_enable_d3d9=false angle_enable_d3d11=false angle_enable_null=false" || exit /b 1
 call git apply -p0 ..\angle.patch || exit /b 1
-call autoninja -C out/ReleaseDX libEGL libGLESv2 libGLESv1_CM || exit /b 1
-rem call autoninja -C out/ReleaseGL libEGL libGLESv2 libGLESv1_CM || exit /b 1
+rem call autoninja -C out/ReleaseDX libEGL libGLESv2 libGLESv1_CM || exit /b 1
+call autoninja -C out/ReleaseGL libEGL libGLESv2 libGLESv1_CM || exit /b 1
 rem call autoninja -C out/Debug libEGL libGLESv2 libGLESv1_CM || exit /b 1
 popd
 
 rem *** prepare output folder ***
 
-mkdir angle
-mkdir angle\bin
-mkdir angle\lib
-mkdir angle\include
+rmdir /q /s bin\GL 1>nul 2>nul
 
-copy /y angle.src\.git\refs\heads\main angle\commit.txt 1>nul 2>nul
+mkdir bin\GL\bin
+mkdir bin\GL\lib
+mkdir bin\GL\include
 
-copy /y "%ProgramFiles(x86)%\Windows Kits\10\Redist\D3D\x64\d3dcompiler_47.dll" angle\bin 1>nul 2>nul
+copy /y angle.src\.git\refs\heads\main bin\GL\commit.txt 1>nul 2>nul
 
-copy /y angle.src\out\Release\libEGL.dll       angle\bin        1>nul 2>nul
-copy /y angle.src\out\Release\libGLESv1_CM.dll angle\bin        1>nul 2>nul
-copy /y angle.src\out\Release\libGLESv2.dll    angle\bin        1>nul 2>nul
+copy /y "%ProgramFiles(x86)%\Windows Kits\10\Redist\D3D\x64\d3dcompiler_47.dll" bin\GL\bin 1>nul 2>nul
 
-copy /y angle.src\out\Release\libEGL.dll.lib       angle\lib    1>nul 2>nul
-copy /y angle.src\out\Release\libGLESv1_CM.dll.lib angle\lib    1>nul 2>nul
-copy /y angle.src\out\Release\libGLESv2.dll.lib    angle\lib    1>nul 2>nul
+copy /y angle.src\out\ReleaseGL\libEGL.dll       bin\GL\bin        1>nul 2>nul
+copy /y angle.src\out\ReleaseGL\libGLESv1_CM.dll bin\GL\bin        1>nul 2>nul
+copy /y angle.src\out\ReleaseGL\libGLESv2.dll    bin\GL\bin        1>nul 2>nul
 
-xcopy /D /S /I /Q /Y angle.src\include\KHR   angle\include\KHR   1>nul 2>nul
-xcopy /D /S /I /Q /Y angle.src\include\EGL   angle\include\EGL   1>nul 2>nul
-xcopy /D /S /I /Q /Y angle.src\include\GLES  angle\include\GLES  1>nul 2>nul
-xcopy /D /S /I /Q /Y angle.src\include\GLES2 angle\include\GLES2 1>nul 2>nul
-xcopy /D /S /I /Q /Y angle.src\include\GLES3 angle\include\GLES3 1>nul 2>nul
+copy /y angle.src\out\ReleaseGL\libEGL.dll.lib       bin\GL\lib    1>nul 2>nul
+copy /y angle.src\out\ReleaseGL\libGLESv1_CM.dll.lib bin\GL\lib    1>nul 2>nul
+copy /y angle.src\out\ReleaseGL\libGLESv2.dll.lib    bin\GL\lib    1>nul 2>nul
 
-del /Q /S angle\include\*.clang-format 1>nul 2>nul
+xcopy /D /S /I /Q /Y angle.src\include\KHR   bin\GL\include\KHR   1>nul 2>nul
+xcopy /D /S /I /Q /Y angle.src\include\EGL   bin\GL\include\EGL   1>nul 2>nul
+xcopy /D /S /I /Q /Y angle.src\include\GLES  bin\GL\include\GLES  1>nul 2>nul
+xcopy /D /S /I /Q /Y angle.src\include\GLES2 bin\GL\include\GLES2 1>nul 2>nul
+xcopy /D /S /I /Q /Y angle.src\include\GLES3 bin\GL\include\GLES3 1>nul 2>nul
+
+del /Q /S bin\GL\include\*.clang-format 1>nul 2>nul
 
 rem *** done ***
 rem output is in angle folder
 
 if "%GITHUB_WORKFLOW%" neq "" (
-  set /p ANGLE_COMMIT=<angle\commit.txt
+  set /p ANGLE_COMMIT=<bin\GL\commit.txt
 
   for /F "skip=1" %%D in ('WMIC OS GET LocalDateTime') do (set LDATE=%%D & goto :dateok)
   :dateok
